15fcde6db198e31389ef072e392c32834d5c584c
rakaishy <alexhander2002@gmail.com>
2022-06-29 19:01:18 -0700

    feat: adjusted initial request

--- main.py
+++ main.py
@@ -1,54 +1,60 @@
 import requests
 import json
+import datetime
 
 url = 'https://eu5.api.enterprisedata.slb.com/api/storage/v2/records'
+dataPartitionId = 'sandbox-weu-des-prod-testing-e'
 
 body = [
-        {
-          "kind": "BASE_KIND",
-          "version": 1562066009929332,
-          "acl": {
+    {
+        "kind": "BASE_KIND",
+        "version": 1562066009929332,
+        "acl": {
             "owners": ['data.default.viewers@' + dataPartitionId + '.enterprisedata.slb.com'],
             "viewers": ['data.default.viewers@' + dataPartitionId + '.enterprisedata.slb.com']
-          },
-          "legal": {
+        },
+        "legal": {
             "legaltags": [dataPartitionId + '-default-legal'],
             "otherRelevantDataCountries": ['US'],
             "status": 'compliant'
-          },
-          "createTime": datetime,
-          "createUser": userID,
-          "modifyTime": datetime,
-          "modifyUser": userID,
-          "ancestry": {
+        },
+        "createTime": datetime.datetime.now(),
+        "createUser": 'aguerrero25@slb.com',
+        "modifyTime": datetime.datetime.now(),
+        "modifyUser": 'aguerrero25@slb.com',
+        "ancestry": {
             "parents": []
-          },
-          "meta": [],
-          "data": {
-            "Name": collectionList.name,
-            "Description": collectionList.desc,
+        },
+        "meta": [],
+        "data": {
+            "Name": 'OSDU Performance Collection',
+            "Description": 'Performance related collection',
             "CreationDateTime": '2020-02-13T09:13:15.55Z',
             "Tags": ['Example Tags'],
             "SubmitterName": 'Example SubmitterName',
             "BusinessActivities": ['Example BusinessActivities'],
             "AuthorIDs": ['Example AuthorIDs'],
-            "MemberIDs": recordIds,
+            "MemberIDs": [],
             "PurposeID": 'namespace:reference-data--CollectionPurpose:Project:',
             "ParentCollectionID":
-              'namespace:work-product-component--PersistedCollection:PersistedCollection-911bb71f-06ab-4deb-8e68-b8c9229dc76b:',
+                'namespace:work-product-component--PersistedCollection:PersistedCollection-911bb71f-06ab-4deb-8e68-b8c9229dc76b:',
             "HomogeneousMemberKind": 'osdu:wks:work-product-component--SeismicHorizon',
             "Author": 'Example Author',
             "ExtensionProperties": {}
-          }
         }
-      ]
+    }
+]
 
 with open('resources/collections.json') as collections:
     collections_data = json.load(collections);
 
 
+def create_request(data_partition_id):
+    create = requests.put(url, data=body)
+
+
 def update_request(data_partition_id):
-    req = requests.put()
+    update = requests.put()
 
 
 print(collections_data)
